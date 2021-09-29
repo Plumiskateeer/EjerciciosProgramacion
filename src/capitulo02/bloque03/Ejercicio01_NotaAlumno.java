0b112dd61bb414aa863976bc268d626fe1155289
Angel <angelote2229914@gmail.com>
2021-09-29 11:10:53 +0200

    Subida bloque 3

--- src/capitulo02/bloque03/Ejercicio01_NotaAlumno.java
+++ src/capitulo02/bloque03/Ejercicio01_NotaAlumno.java
@@ -6,34 +6,39 @@ public class Ejercicio01_NotaAlumno {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Introduce la nota obtenida: ");
 		int nota = sc.nextInt();
-		
+
 		switch (nota) {
-		case : {
-			
+		case 1: case 2: {
 			System.out.println("Muy deficiente");
+			break;
 		}
-		case nota<5: {
-					
-					yield type;
-				}
-		case nota<7: {
-			
-			yield type;
+		case 3: case 4: {
+			System.out.println("Insuficiente");
+			break;
 		}
-		case nota<9: {
-			
-			yield type;
+		case 5: {
+			System.out.println("Suficiente");
+			break;
+		}
+		case 6: {
+			System.out.println("Bien");
+			break;
+		}
+		case 7: case 8:  {
+			System.out.println("Notable");
+			break;
+		}
+		case 9: case 10:{
+			System.out.println("Sobresaliente");
+			break;
 		}
 		default:
 			throw new IllegalArgumentException("Unexpected value: " + nota);
 		}
-		
-		
 	}
-
 }
---
+++ src/capitulo02/bloque03/Ejercicio02_Calculadora.java
@@ -0,0 +1,66 @@
+package capitulo02.bloque03;
+
+import java.net.StandardSocketOptions;
+import java.util.Scanner;
+
+public class Ejercicio02_Calculadora {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int op;
+        do {
+            System.out.println("BIENVENIDO A LA CALCULADORA");
+            System.out.println(".--------------------.");
+            System.out.println("1 para modulo division");
+            System.out.println("2 para calcular la raiz cuadrada");
+            System.out.println("3 para la potencia");
+            System.out.println(".--------------------.");
+            System.out.println("Elige la operacion a realizar, 0 para salir: ");
+            op = sc.nextInt();
+
+
+            switch (op){
+                case 1: {
+                    System.out.println("El modulo de la division es " + modDiv());
+                    break;
+                }
+                case 2: {
+                    System.out.println("El resultado de la raiz es: " + raiz());
+                    break;
+                }case 3: {
+                    System.out.println("El resultado de la potencia es: " + potencia());
+                    break;
+                }
+
+            }
+        }while(op!=0);
+    }
+
+    public static int modDiv(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el dividendo: ");
+        int dividendo = sc.nextInt();
+        System.out.println("Introduce el divisor: ");
+        int divisor = sc.nextInt();
+
+        return dividendo%divisor;
+    }
+    public static double raiz(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce el numero al que calcular la raiz: ");
+        double n = sc.nextDouble();
+
+        return Math.sqrt(n);
+
+    }
+    public static double potencia(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce la base de la potencia: ");
+        int b = sc.nextInt();
+        System.out.println("Introduce el exponente de la potencia: ");
+        int ex = sc.nextInt();
+
+        return Math.pow(b,ex);
+    }
+}
