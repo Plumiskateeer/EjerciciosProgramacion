f9e97699eb6073bf64656bd15043621ee5e9040d
amjyebenes <angelote2229914@gmail.com>
2021-10-21 14:36:28 +0200

    primer examen

--- src/examenes/examen20211021/Ejercicio03.java
+++ src/examenes/examen20211021/Ejercicio03.java
@@ -6,10 +6,12 @@ public class Ejercicio03 {
         int cont = 0;
         // mientras cont menor que 10, sigo creando el num aleatorio y lo imprimo si es par
         while (cont < 10) {
-            int num = ((int) (Math.round((Math.random() * (30 - 20)) + 20)));
+            int num = ((int) (Math.round((Math.random() * (50 - 20)) + 20)));
             if (num % 2 == 0) {
-                System.out.print(num + " ");
-                cont++;
+                if (num <= 30 || num >= 40) {
+                    System.out.print(num + " ");
+                    cont++;
+                }
             }
         }
     }
