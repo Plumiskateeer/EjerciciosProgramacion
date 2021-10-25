afb1dd29d21ba1b1fc2a890099275c9abfd8d4af
Angel <angelote2229914@gmail.com>
2021-10-25 19:28:33 +0200

    update ej 6 bloque 2 cap 5

--- src/capitulo05/bloque02/Ejercicio06_DesplazarDireccion.java
+++ src/capitulo05/bloque02/Ejercicio06_DesplazarDireccion.java
@@ -5,41 +5,37 @@ import javax.swing.*;
 public class Ejercicio06_DesplazarDireccion {
     public static void main(String[] args) {
         int[] matriz = new int[5];
+        int salvar;
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de desplazamientos: "));
+        int desp = Integer.parseInt(JOptionPane.showInputDialog("Hacia la izquierda (0) Hacia la derecha (1): "));
 
-        // inicializo e imprimo
         for (int i = 0; i < matriz.length; i++) {
             matriz[i] = (int) (Math.round(Math.random() * (100)));
             System.out.print(matriz[i] + " ");
         }
         System.out.println();
 
-        int salvar;
-        int num = Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de desplazamientos: "));
-        int desp = Integer.parseInt(JOptionPane.showInputDialog("Hacia la izquierda (0) Hacia la derecha (1): "));
-
-
-        // si el usuario me ha indicado der hago der, 1, si el usuario me indica izq hago izq, 0.
-        if (desp == 1) {
-            for (int j = 0; j < num; j++) { // hacia la derecha se desplaza num veces
-                salvar = matriz[matriz.length - 1];
-                for (int i = matriz.length - 1; i >= 0; i--) {
-                    if ((i - 1) >= 0)
-                        matriz[i] = matriz[i - 1];
-                }
-                matriz[0] = salvar;
-            }
-        } else {
-            for (int j = 0; j < (matriz.length - num); j++) { // hacia la izquierda se desplaza matriz.length - num veces, da la vuelta
-                salvar = matriz[matriz.length - 1];
-                for (int i = matriz.length - 1; i >= 0; i--) {
-                    if ((i - 1) >= 0)
-                        matriz[i] = matriz[i - 1];
-                }
-                matriz[0] = salvar;
-            }
+        // string que indica resultado
+        String direccion = "Desplazamiento " + num + " veces a la derecha";
+        // si pide hacia la izquierda se desplaza matriz.length - num veces, es circular
+        // si pide hacia la derecha se deja tal cual
+        if (desp == 0) {
+            num = matriz.length - num;
+            direccion = "Desplazamiento " + num + " veces a la izquierda";
         }
 
-        // imprimo el resultado
+        // este metodo es el encargado de desplazar la matriz
+        for (int j = 0; j < num; j++) { // hacia la derecha se desplaza num veces
+            salvar = matriz[matriz.length - 1];
+            for (int i = matriz.length - 1; i >= 0; i--) {
+                if ((i - 1) >= 0)
+                    matriz[i] = matriz[i - 1];
+            }
+            matriz[0] = salvar;
+        }
+
+        System.out.println(direccion);
+        // imprimo solucion
         for (int i : matriz)
             System.out.print(i + " ");
     }
