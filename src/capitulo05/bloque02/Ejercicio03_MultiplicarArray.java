aa3ab700c51c70a1348ce2075481895b560f4db9
amjyebenes <angelote2229914@gmail.com>
2021-10-25 12:22:29 +0200

    cambio ej 3 bloque 2 cap 5

--- src/capitulo05/bloque02/Ejercicio03_MultiplicarArray.java
+++ src/capitulo05/bloque02/Ejercicio03_MultiplicarArray.java
@@ -4,22 +4,22 @@ import javax.swing.*;
 
 public class Ejercicio03_MultiplicarArray {
     public static void main(String[] args){
-        int[] m1 = new int[150];
+        int[] matriz = new int[150];
 
         System.out.println("Matriz original");
-        for (int i = 0; i < m1.length; i++) {
-            m1[i] = (int) Math.round(Math.random() * (100));
-            System.out.print(m1[i] + " ");
+        for (int i = 0; i < matriz.length; i++) {
+            matriz[i] = (int) Math.round(Math.random() * (100));
+            System.out.print(matriz[i] + " ");
         }
 
         int num = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero para multiplicar el array: "));
 
-        for(int i = 0; i < m1.length; i++){
-            m1[i] *= num;
-        }
         System.out.println("\nMatriz multiplicada por " + num);
-        for(int i : m1){
-            System.out.print(i + " ");
+        for(int i : matriz){
+            i *= num;
+            System.out.print( i+ " ");
         }
+
+
     }
 }
