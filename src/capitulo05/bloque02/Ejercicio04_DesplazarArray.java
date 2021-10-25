a784e5b15fc27aafe47b1a8aaebc2f675ca94659
amjyebenes <angelote2229914@gmail.com>
2021-10-25 12:25:05 +0200

    cambio ej 4 bloque 2 cap 5

--- src/capitulo05/bloque02/Ejercicio04_DesplazarArray.java
+++ src/capitulo05/bloque02/Ejercicio04_DesplazarArray.java
@@ -4,11 +4,12 @@ public class Ejercicio04_DesplazarArray {
     public static void main(String[] args) {
         int[] matriz = new int[5];
 
+        System.out.println("Matriz original: ");
         for (int i = 0; i < matriz.length; i++) {
             matriz[i] = (int) (Math.round(Math.random() * (100)));
             System.out.print(matriz[i] + " ");
         }
-        System.out.println();
+
         int ultimo = matriz[matriz.length - 1];
 
         for (int i = matriz.length - 1; i >= 0; i--) {
@@ -16,6 +17,8 @@ public class Ejercicio04_DesplazarArray {
                 matriz[i] = matriz[i - 1];
         }
         matriz[0] = ultimo;
+
+        System.out.println("\nMatriz rotada: ");
         for (int i : matriz)
             System.out.print(i + " ");
     }
