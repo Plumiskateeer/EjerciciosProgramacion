d4c32cf507d03a45371c05b680fa55d6fea38ac1
amjyebenes <angelote2229914@gmail.com>
2021-10-29 10:41:07 +0200

    F caracol

---
+++ src/EjerciciosExtra/Ejercicio01_ParesAdelante.java
@@ -0,0 +1,11 @@
+package EjerciciosExtra;
+
+public class Ejercicio01_ParesAdelante {
+    public static void main(String[] args){
+        int []  numeros = new int[20];
+
+        for (int i = 0; i < numeros.length; i++) {
+
+        }
+    }
+}
