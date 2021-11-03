6c5d273f2564815fcc8bbae0b2340120411aca18
amjyebenes <angelote2229914@gmail.com>
2021-11-03 09:35:07 +0100

    Actualizacion cola

--- src/EjerciciosExtra/Ejercicio01_ParesAdelante.java
+++ src/EjerciciosExtra/Ejercicio01_ParesAdelante.java
@@ -5,7 +5,6 @@ public class Ejercicio01_ParesAdelante {
         int []  numeros = new int[20];
 
         for (int i = 0; i < numeros.length; i++) {
-
             numeros[i] = (int)(Math.round(Math.random()*100)); // numeros.length
 
         }
--- src/capitulo05/bloque05/Ejercicio02_Supermercado/Principal.java
+++ src/capitulo05/bloque05/Ejercicio02_Supermercado/Principal.java
@@ -2,6 +2,8 @@ package capitulo05.bloque05.Ejercicio02_Supermercado;
 
 public class Principal {
     public static void main(String[] args) throws InterruptedException {
+        // He adaptado este ejercicio porque empece a hacerlo con arrays y me quedo de una manera muy rara,
+        // he realizado una cola que va atendiendo y recibiendo clientes hasta el infinito, usando Threads concurrentes
         Cola c = new Cola();
         AnadirCliente anc = new AnadirCliente(c);
         AtenderCliente atc = new AtenderCliente(c);
