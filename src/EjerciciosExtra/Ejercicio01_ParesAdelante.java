74418937f95bc94d8e72994258c507a2195a8c7f
amjyebenes <angelote2229914@gmail.com>
2021-11-03 11:30:58 +0100

    UPGRADE

--- src/EjerciciosExtra/Ejercicio01_ParesAdelante.java
+++ src/EjerciciosExtra/Ejercicio01_ParesAdelante.java
@@ -3,10 +3,30 @@ package EjerciciosExtra;
 public class Ejercicio01_ParesAdelante {
     public static void main(String[] args){
         int []  numeros = new int[20];
+        int nPares = 0;
+        int [] pares = new int[numeros.length];
+        int nImpares = 0;
+        int [] impares = new int[numeros.length];
 
+        System.out.println("Matriz original");
         for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = (int)(Math.round(Math.random()*100)); // numeros.length
-
+            numeros[i] = (int)(Math.round(Math.random()*100));
+            System.out.print(numeros[i] + " ");
+            if(numeros[i]%2==0) {
+                pares[nPares] = numeros[i];
+                nPares++;
+            }else{
+                impares[nImpares] = numeros[i];
+                nImpares++;
+                }
         }
+
+        System.arraycopy(pares, 0, numeros, 0, nPares);
+        System.arraycopy(impares, 0, numeros, nPares, nImpares);
+
+        System.out.println("\nMatriz ordenada pares a la derecha");
+        for(int i : numeros)
+            System.out.print(i + " ");
+
     }
 }
