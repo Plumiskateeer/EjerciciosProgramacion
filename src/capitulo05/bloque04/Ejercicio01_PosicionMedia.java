813e7545545d9f3bce11c43026f8f5faadf43a30
amjyebenes <angelote2229914@gmail.com>
2021-11-10 11:25:39 +0100

    ctrl + alt + L

--- src/capitulo05/bloque04/Ejercicio01_PosicionMedia.java
+++ src/capitulo05/bloque04/Ejercicio01_PosicionMedia.java
@@ -20,11 +20,10 @@ public class Ejercicio01_PosicionMedia {
 
     public static void encontrarElementoMedio(int[] matriz) {
         int valorPosMedia;
-        if (matriz.length % 2 == 0) { // compruebo el tamaño y dependiendo de si es par o no igualo a su valor intermedio
-            valorPosMedia = -1;
-        } else {
-            valorPosMedia = matriz[(matriz.length - 1) / 2];
-        }
+        // compruebo el tamaño y dependiendo de si es par o no igualo a su valor intermedio
+        if (matriz.length % 2 == 0) valorPosMedia = -1;
+        else valorPosMedia = matriz[(matriz.length - 1) / 2];
+
         System.out.println("\nEl valor que se encuentra en la posicion intermedia es: " + valorPosMedia);
     }
 }
