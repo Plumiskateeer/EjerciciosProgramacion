99305a2256b0ec370938c7ec4f9308f68d17e489
Angel <angelote2229914@gmail.com>
2021-11-20 00:27:29 +0100

    sudo apt update

--- src/capitulo05/bloque07/Ejercicio01_ArrayToString.java
+++ src/capitulo05/bloque07/Ejercicio01_ArrayToString.java
@@ -1,23 +1,23 @@
 package capitulo05.bloque07;
 
 public class Ejercicio01_ArrayToString {
-    public static void main(String[] args){
-        int[]  matriz = new int[10];
+    public static void main(String[] args) {
+        int[] matriz = new int[100];
         inicializarMatriz(matriz);
         System.out.println(arrayToString(matriz));
     }
 
-    public static void inicializarMatriz(int[] matriz){
+    public static void inicializarMatriz(int[] matriz) {
         for (int i = 0; i < matriz.length; i++) {
-            matriz[i] = (int)(Math.round(Math.random()*10));
+            matriz[i] = (int) (Math.round(Math.random() * 100));
         }
     }
-    public static String arrayToString(int[] matriz){
+
+    public static String arrayToString(int[] matriz) {
         StringBuilder sb = new StringBuilder();
         sb.append("La matriz es: ");
-        for(int i : matriz){
-            sb.append(i);
-            sb.append(" ");
+        for (int i : matriz) {
+            sb.append((char) i);
         }
         return sb.toString();
     }
