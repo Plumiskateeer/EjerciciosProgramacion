599693f9eade5841578b2e7d333657a4e21578ef
Angel <angelote2229914@gmail.com>
2021-11-24 20:09:58 +0100

    sudo apt update

--- src/capitulo05/bloque07/Ejercicio05_RemoveInteger.java
+++ src/capitulo05/bloque07/Ejercicio05_RemoveInteger.java
@@ -9,13 +9,12 @@ public class Ejercicio05_RemoveInteger {
         System.out.println("Array original\n" + Arrays.toString(matriz));
         int num = 4;
         System.out.println("Matriz sin el numero " + num);
-        matriz = Arrays.copyOf(removeInteger(matriz, num), removeInteger(matriz, num).length);
+        matriz = removeInteger(matriz, num);
         System.out.println("Array editada\n" + Arrays.toString(matriz));
     }
 
     public static int[] removeInteger(int[] matriz, int borrar){
-        int[] aux = Arrays.copyOf(matriz,
-                matriz.length - Ejercicio04_CountOcurrences.countOcurrences(matriz, borrar));
+        int[] aux = Arrays.copyOf(matriz, matriz.length - Ejercicio04_CountOcurrences.countOcurrences(matriz, borrar));
 
         int pos = 0;
         for (int i = 0; i < matriz.length; i++)
