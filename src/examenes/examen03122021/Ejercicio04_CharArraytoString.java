70f7d5175ddacfbb3101ebd2155dae278a2b5243
amjyebenes <angelote2229914@gmail.com>
2021-12-03 10:32:22 +0100

    Examen 03/12/2021

--- src/examenes/examen03122021/Ejercicio04_CharArraytoString.java
+++ src/examenes/examen03122021/Ejercicio04_CharArraytoString.java
@@ -4,17 +4,19 @@ import java.util.Locale;
 
 public class Ejercicio04_CharArraytoString {
     public static void main(String[] args) {
-        char[] array = {'H', 'O', 'L', 'A', ' ', 'M', 'U', 'N', 'D', 'O'};
+        char[] array = {'H', 'o', 'l', 'a', ' ', 'M', 'u', 'n', 'd', 'o'};
         System.out.println("Array original en minusculas: ");
         System.out.println(array);
         System.out.println("Array en minusculas y en String: \n" + minusculas(array));
     }
 
     public static String minusculas(char frase[]) {
-        StringBuilder sb = new StringBuilder();
+        String sb = "";
         for (int i = 0; i < frase.length; i++) {
-            sb.append(frase[i]);
+            if(frase[i]>= 65 && frase[i] <= 90)
+            sb+=(char)(frase[i] + 32);
+            else sb+=(frase[i]);
         }
-        return sb.toString().toLowerCase(Locale.ROOT);
+        return sb;
     }
 }
