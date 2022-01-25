6b1ed7472d0ac3ce041b84806b97946a19af4da8
amjyebenes <angelote2229914@gmail.com>
2022-01-25 12:34:46 +0100

    antes de los cambios

--- src/capitulo04/SimulacroExamenSpaceInvaders/Guerra.java
+++ src/capitulo04/SimulacroExamenSpaceInvaders/Guerra.java
@@ -5,17 +5,18 @@ import java.awt.*;
 
 public class Guerra {
     public static void main(String[]args){
-//        CampoBatalla campo = CampoBatalla.getInstance();
-//        campo.pelea();
-        JFrame ventana = new JFrame("Space Invaders");
-        ventana.setBounds(0,0,800,600);
-        
-        ventana.getContentPane().setLayout(new BorderLayout());
-        
-        MiCanvas canvas = new MiCanvas(creaPersonajes());
-        ventana.getContentPane().add(canvas,BorderLayout.CENTER);
-        
-        ventana.setVisible(true);
+        CampoBatalla campo = CampoBatalla.getInstance();     // ESTE ES EL MAIN DE LA ENTREGA DEL CLASSROOM
+        campo.pelea();
+//
+//        JFrame ventana = new JFrame("Space Invaders");
+//        ventana.setBounds(0,0,800,600);
+//
+//        ventana.getContentPane().setLayout(new BorderLayout());
+//
+//        MiCanvas canvas = new MiCanvas(creaPersonajes());
+//        ventana.getContentPane().add(canvas,BorderLayout.CENTER);
+//
+//        ventana.setVisible(true);
     }
 
 }
