9749105e6cbaa64e80d57967878939be375f0354
amjyebenes <angelote2229914@gmail.com>
2022-02-16 17:36:27 +0100

    l

---
+++ src/capitulo06/wrappers/Ejercicio03_100.java
@@ -0,0 +1,10 @@
+package capitulo06.wrappers;
+
+public class Ejercicio03_100 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println("Decodificación base decimal " + i +": " + Integer.toHexString(i));
+        }
+
+    }
+}
---
+++ src/capitulo06/wrappers/Propiedades.java
@@ -0,0 +1,97 @@
+package capitulo06.wrappers;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Propiedades {
+    private static Properties propiedades = null;
+
+    public Propiedades () {
+        super();
+    }
+
+    /**
+     *
+     * @return
+     */
+    private static Properties getPropiedades() {
+        if (propiedades == null) {
+            propiedades = new Properties();
+
+            try {
+
+                // Otra forma de leer el fichero de propiedades
+                File file = new File("./src/capitulo06/wrappers/ejemplo.properties");
+                System.out.println("Fichero encontrado: " + file.exists());
+                propiedades.load(new FileReader(file));
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return propiedades;
+    }
+
+    /**
+     *
+     * @param nombrePropiedad
+     * @return
+     */
+    public static String getProperty(String nombrePropiedad) {
+        return getPropiedades().getProperty(nombrePropiedad);
+    }
+
+
+    /**
+     *
+     * @param nombrePropiedad
+     * @return
+     */
+    public static int getIntProperty (String nombrePropiedad) {
+        return Integer.parseInt(getPropiedades().getProperty(nombrePropiedad));
+    }
+
+
+    /**
+     *
+     * @param nombrePropiedad
+     * @return
+     */
+    public static Float getFloatProperty (String nombrePropiedad) {
+        return Float.parseFloat(getPropiedades().getProperty(nombrePropiedad));
+    }
+
+    /**
+     *
+     * @param nombrePropiedad
+     * @return
+     */
+    public static Boolean getBooleanProperty (String nombrePropiedad) {
+        return Boolean.parseBoolean(getPropiedades().getProperty(nombrePropiedad));
+    }
+
+
+
+    /**
+     *
+     * @param args
+     */
+    public static void main (String args[]) {
+        String usuario = Propiedades.getProperty("USUARIO");
+        Float password = Propiedades.getFloatProperty("PASSWORD");
+        int id = Propiedades.getIntProperty("ID_USUARIO");
+        boolean activo = Propiedades.getBooleanProperty("VIVO");
+
+        System.out.println("Usuario leído del fichero de propiedades: " + usuario);
+        System.out.println("Password leído del fichero de propiedades: " + password);
+        System.out.println("Id de usuario leído del fichero de propiedades: " + id);
+        System.out.println("Estado de usuario " + activo);
+
+    }
+}
